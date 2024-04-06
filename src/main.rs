58993771c0b7cbda4882aa0e2c106a28e79148f3
Yakhya Dabo <yakhyadabo@gmail.com>
2024-04-06 17:10:59 +0100

    [model] Create structs, package

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+mod model;  // Import Module model
+
+use crate::model::company::Company;
+use crate::model::client::Client;
 
 fn my_print() {
     println!("Hello, world!");
 }
 
 fn main() {
+    let _comp = Company {};
+    let _client = Client {};
     my_print()
 }
